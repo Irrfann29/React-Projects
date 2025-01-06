aafd3c1b62dd8f947fc9ff8a3c4ca3cf4205b657
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-01-06 19:02:58 +0530

    Added Tailwind CSS

--- React Project/src/App.jsx
+++ React Project/src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
   }
   return (
     <div>
-      <h1>Hello {name}</h1>
-      <button onClick={click}>Click Me</button>
+      <h1 className="text-4xl m-3 ">Hello {name}</h1>
+      <button onClick={click} className="bg-blue-300 rounded-xl mx-5 cursor-pointer">Click Me</button>
     </div>
   )
 }
