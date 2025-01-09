62f41a20d4aacd693cfbc34a2f05da543919fd30
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-01-09 21:21:59 +0530

    Minor Changes

--- React Project/src/App.jsx
+++ React Project/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-4xl m-3 ">Hello {name}</h1>
-      <button onClick={click} className="bg-blue-300 rounded-xl mx-5 cursor-pointer">Click Me</button>
+      <button onClick={click} className="bg-orange-400 rounded-xl mx-5 cursor-pointer">Click Me</button>
     </div>
   )
 }
