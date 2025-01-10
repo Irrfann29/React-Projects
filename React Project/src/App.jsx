c9778281a047392c5c177a80f47ba089a5295e3a
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-01-10 21:59:10 +0530

    Minor Changes

--- React Project/src/App.jsx
+++ React Project/src/App.jsx
@@ -1,13 +1,31 @@
 // import React from 'react'
+import { useState } from "react"
 
 const App = () => {
-  const name = "Irfan"
-  const click = ()=>{
-    console.log("Button Clicked")
-  }
+  let [color,setColor] = useState("olive")
   return (
-    <div className="h-screen w-full bg-red-500">
-      <div className="fixed bottom-5 flex items-center justify-center inset-x-1">hello
+    <div className="h-screen w-full" style={{backgroundColor:color}}>
+      <div className="fixed bottom-5 flex flex-wrap items-center justify-center gap-20 inset-x-1  bg-emerald-800 p-1 rounded-xl ">
+        <div onClick={()=>setColor("red")} className="flex flex-wrap px-5 py-2 text-white rounded-2xl text-xl cursor-pointer" style={{backgroundColor:"red"}}>
+          <button>Red</button>
+        </div>
+        <div onClick={()=>setColor("olive")} className="flex flex-wrap px-5 py-2 text-white rounded-2xl text-xl cursor-pointer" style={{backgroundColor:"olive"}}>
+          <button>Olive</button>
+        </div>
+        <div onClick={()=>setColor("black")} className="flex flex-wrap px-5 py-2 text-white rounded-2xl text-xl cursor-pointer" style={{backgroundColor:"black"}}>
+          <button>Black</button>
+        </div>
+        <div onClick={()=>setColor("purple")} className="flex flex-wrap px-5 py-2 text-white rounded-2xl text-xl cursor-pointer" style={{backgroundColor:"purple"}}>
+          <button>Purple</button>
+        </div>
+        <div onClick={()=>setColor("blue")} className="flex flex-wrap px-5 py-2 text-white rounded-2xl text-xl cursor-pointer" style={{backgroundColor:"blue"}}>
+          <button>blue</button>
+        </div>
+        <div onClick={()=>setColor("green")} className="flex flex-wrap px-5 py-2 text-white rounded-2xl text-xl cursor-pointer" style={{backgroundColor:"green"}}>
+          <button>Green</button>
+        </div>
+        
+
 
       </div>
     </div>
