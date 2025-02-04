12a117772c117e45808001df75644f3b7ad11201
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-02-04 18:51:33 +0530

    Minor Changes

--- Currency Calculator/src/App.jsx
+++ Currency Calculator/src/App.jsx
@@ -1,7 +1,11 @@
 
 
 function App() {
-  return "Hello World"
+  return <>
+    <h1 className="bg-red-500">Currency Calculator</h1>
+    <h2 className="text-3xl bg-black text-white">Hello Ji</h2>
+  
+  </>
 }
 
 export default App
