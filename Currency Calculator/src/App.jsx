9b3fb7a02c94d6fccfe58f65a09497c48bdc1a7f
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-02-06 08:04:58 +0530

    Minor Changes

--- Currency Calculator/src/App.jsx
+++ Currency Calculator/src/App.jsx
@@ -1,11 +1,11 @@
+import React from 'react'
 
-
-function App() {
-  return <>
-    <h1 className="bg-red-500">Currency Calculator</h1>
-    <h2 className="text-3xl bg-black text-white">Hello Ji</h2>
-  
-  </>
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-extrabold bg-emerald-200'>Hello world</h1>
+    </div>
+  )
 }
 
 export default App
