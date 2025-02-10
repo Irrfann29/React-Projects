3e7d4d80afaa62c32b08d7032d912c7bc531d982
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-02-10 19:19:55 +0530

    Minor Changes

--- Tic Tac Toe/src/App.jsx
+++ Tic Tac Toe/src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
 
+function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
-      <h1 className='bg-red-500'>My self Irfan</h1>
+      <div className="board-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
     </>
-  )
+  );
 }
 
 export default App
