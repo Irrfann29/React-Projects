e1762b1861562be52b5d208281b4823b3f1736e4
Irrfann29 <mrirfanhashmi2303@gmail.com>
2025-02-11 19:15:22 +0530

    Minor Changes

--- Tic Tac Toe/src/Components/TicTacToe.jsx
+++ Tic Tac Toe/src/Components/TicTacToe.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TicTactoe.css'
+import cross from '../assets/cross.png'
+import circle from '../assets/circle.png'
 
+let data = ['','','','','','','','',''];
 const TicTacToe = () => {
+    let [count,setCount]=useState(0);
+    let [lock,setLock]=useState(false);
+
+    const toggle = (e,num)=>{
+        if (lock){
+            return 0;
+        }
+        if (count%2==0){
+            e.target.innerHTML=`<img src=${cross} alt='cross'/>`;
+            data[num]='X';
+            setCount(++count)
+        }
+        else {
+            e.target.innerHTML=`<img src=${circle} alt='cross'/>`;
+            data[num]='O';
+            setCount(++count);
+        }
+
+    }
     return (
         <>
             <div className='view'>
@@ -11,19 +33,19 @@ const TicTacToe = () => {
                     </div>
                     <div className="board">
                         <div className="row1">
-                            <div className="square"></div>
-                            <div className="square"></div>
-                            <div className="square"></div>
+                            <div className="square" onClick={(e)=>{toggle(e,0)}}></div>
+                            <div className="square" onClick={(e)=>{toggle(e,0)}}></div>
+                            <div className="square" onClick={(e)=>{toggle(e,0)}}></div>
                         </div>
                         <div className="row2">
-                            <div className="square"></div>
-                            <div className="square"></div>
-                            <div className="square"></div>
+                            <div className="square" onClick={(e)=>{toggle(e,0)}}></div>
+                            <div className="square" onClick={(e)=>{toggle(e,0)}}></div>
+                            <div className="square" onClick={(e)=>{toggle(e,0)}}></div>
                         </div>
                         <div className="row3">
-                            <div className="square"></div>
-                            <div className="square"></div>
-                            <div className="square"></div>
+                            <div className="square" onClick={(e)=>{toggle(e,0)}}></div>
+                            <div className="square" onClick={(e)=>{toggle(e,0)}}></div>
+                            <div className="square" onClick={(e)=>{toggle(e,0)}}></div>
                         </div>
 
 
